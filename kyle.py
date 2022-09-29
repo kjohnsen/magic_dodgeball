db7840c01c4c33ac938b18d18519fa2e060e3e10
Kyle Johnsen <kyle@kjohnsen.org>
2022-09-29 16:01:23 -0400

    finish attacks

--- kyle.py
+++ kyle.py
@@ -1,3 +1,4 @@
+from math import sin
 from player import Player
 
 class Kyle(Player):
@@ -6,9 +7,14 @@ class Kyle(Player):
         return v
 
     def traj2(self, pos, v):
-        v = [5*s for s in self.forward]
-        return v
+        vx = 5*self.forward[0]
+        vy = 5*sin(pos[0]/50)
+        return [vx, vy]
 
     def traj3(self, pos, v):
-        v = [5*s for s in self.forward]
-        return v
+        # target is 400 pixels away from wherever the player is
+        xtarget = self.center[0] + 400 * self.forward[0]
+        slowdown = 40
+        vx = (xtarget - pos[0]) / slowdown
+        vy = self.v[1]
+        return [vx, vy]
